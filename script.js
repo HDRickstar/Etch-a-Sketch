5c8a8730a969981a88db9fff618d0239159e925f
Ricardo Reyes <HDRickstar@gmail.com>
2026-03-30 16:01:29 -0500

    added basic function

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-const grid = document.querySelector("container");
+const grid = document.querySelector("#container");
+
 for(let i = 0; i < 16 * 16;i++ ){
     const cell = document.createElement("div");
     cell.classList.add("cell");
+
+    cell.addEventListener("mouseover", () => {
+    cell.style.backgroundColor = "black";
+    });
     grid.appendChild(cell);
 }
+
